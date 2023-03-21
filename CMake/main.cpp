fb0ce4027784c70126be7ab28355b1026d5649c0
Danny Murphy <dmurphy@keplercheuvreux.com>
2023-03-21 15:54:50 +0000

    scripts for convenience, added code versioning

--- CMake/main.cpp
+++ CMake/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include <MyTestProjectConfig.h>
 
-int main() {
+
+//Argc is argcount and argv is arg values
+int main(int argc, char * argv[]) {
     // Standard introduction to every coding language, the simple Hello World program :) 
     std::cout << "Hello, World!\n";
     std::cout << add(72.1f, 145.5f) << "\n";
+    std::cout << argv[0] << "Version:" << MY_TEST_PROJECT_VERSION_MAJOR << "." << MY_TEST_PROJECT_VERSION_MINOR << "\n";
     return 0;
 }
