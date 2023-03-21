8bf37a040a741f936787fda2f77eb7525ffb1013
Danny Murphy <dmurphy@keplercheuvreux.com>
2023-03-21 15:09:55 +0000

    CMake videos 1,2,3

---
+++ CMake/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+int main() {
+    // Standard introduction to every coding language, the simple Hello World program :) 
+    std::cout << "Hello, World!\n";
+    std::cout << add(72.1f, 145.5f) << "\n";
+    return 0;
+}
